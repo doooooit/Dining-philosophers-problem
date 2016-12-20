8e21cd741f4ac70633b68a65491b1affeaf88b52
shangke <shangke01@gmail.com>
2016-12-20 09:48:21 +0800

    之前的用于实现线程同步的是互斥锁，此版本修改为使用信号量 V0.2.1

--- src/bad.c
+++ src/bad.c
@@ -5,41 +5,47 @@
  **************************************/
 
 #include <pthread.h>
+#include <semaphore.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 // 定义全局资源(资源数组)
 #define RESOURCES 5
-pthread_mutex_t mutex[RESOURCES];
+sem_t forks[RESOURCES];
 
 // 定义最大线程数
 #define NUM_THREADS 5
 
-// 线程函数，先获取第一个资源，再请求第二个资源
-void * fetch(void * threadid) {
-    int tid = *((int *) threadid);
-    pthread_mutex_lock(&mutex[tid]);
-    printf("线程 %d 已获取 %d 号资源，正在请求 %d 号资源...\n",
-            tid, tid, (tid + 1) % 5);
+// 描述哲学家资源申请过程的函数，先获取第一个资源，再请求第二个资源
+void * philosopher(void * ID) {
+    int id = *((int *) ID);
+    if (!sem_wait(&forks[id])) {
+        printf("%d 号哲学家：已获取 %d 号叉子，正在请求 %d 号叉子...\n",
+            id, id, (id + 1) % 5);
+    }
+
     sleep(2);
-    pthread_mutex_lock(&mutex[(tid + 1) % 5]);
-    pthread_mutex_unlock(&mutex[tid]);
-    pthread_mutex_unlock(&mutex[(tid + 1) % 5]);
+    sem_wait(&forks[(id + 1) % 5]);
+    sem_post(&forks[id]);
+    sem_post(&forks[(id + 1) % 5]);
 }
 
 int main(int argc, char const *argv[]) {
     // 资源初始化
     for (size_t i = 0; i < RESOURCES; i++) {
-        pthread_mutex_init(&mutex[i], NULL);
+        if (sem_init(&forks[i], 0, 1)) {
+            printf("Error: 资源初始化失败\n");
+            abort();
+        }
     }
 
     pthread_t threadid[NUM_THREADS];
-    int indexes[NUM_THREADS];   // 用来记录线程号
+    int indexes[NUM_THREADS];   // 用来记录线程和资源的序号
 
     for (size_t i = 0; i < NUM_THREADS; i++) {
         indexes[i] = i;
-        int ret = pthread_create(&threadid[i], NULL, fetch
+        int ret = pthread_create(&threadid[i], NULL, philosopher
                                  , (void*) &(indexes[i]));
         if (ret) {
             printf("Error: 创建线程出错\n");
@@ -51,10 +57,6 @@ int main(int argc, char const *argv[]) {
     // 回收线程
     for (size_t i = 0; i < NUM_THREADS; i++) {
         pthread_join(threadid[i], NULL);
-    }
-
-    for (size_t i = 0; i < RESOURCES; i++) {
-        pthread_mutex_destroy(&mutex[i]);
     }
 
     return 0;
