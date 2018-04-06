0785576cbcc5819d29f3ad23157e66701f22c832
shangke <shangke01@gmail.com>
2018-04-06 23:11:05 +0800

    feat: 优化代码

--- src/correct2.cpp
+++ src/correct2.cpp
@@ -1,46 +1,35 @@
 #include "../inc/correct2.h"
 
-extern void *philosopher2(void *ID) {
-    int thisID = *((int *) ID);
-    int leftFork = thisID;
-    int rightFork = (thisID + 1) % RESOURCES;
+extern void* philosopher2(void* ID) {
+    int lowIndex;
+    int highIndex;
+    int thisID = *((int* ) ID);
+
+    if (thisID < ((thisID + 1) % RESOURCES)) {
+        lowIndex = thisID;
+        highIndex = (thisID + 1) % RESOURCES;
+    } else {
+        lowIndex = (thisID + 1) % RESOURCES;
+        highIndex = thisID;
+    }
 
     status[thisID] = "Thinking";
-    usleep(random() % SLEEP_MAX);   // 随机休眠一段时间
+    usleep(random() % SLEEP_MAX); // 随机休眠一段时间
     status[thisID] = "Waiting";
 
-    // 判断资源序号
-    if (leftFork < rightFork) {
-            // 请求资源
-        sem_wait(&forks[leftFork]);
-        heldBy[leftFork] = thisID;
-        sem_wait(&forks[rightFork]);
-        heldBy[rightFork] = thisID;
+    sem_wait(&forks[lowIndex]);
+    heldBy[lowIndex] = thisID;
+    sem_wait(&forks[highIndex]);
+    heldBy[highIndex] = thisID;
 
-        status[thisID] = "Eating";
-        usleep(random() % SLEEP_MAX);   // 随机休眠一段时间
-        status[thisID] = "Terminated";
+    status[thisID] = "Eating";
+    usleep(random() % SLEEP_MAX); // 随机休眠一段时间
+    status[thisID] = "Terminated";
 
-        sem_post(&forks[rightFork]);
-        heldBy[rightFork] = -1;
-        sem_post(&forks[leftFork]);
-        heldBy[leftFork] = -1;
-    }
-    else {
-        sem_wait(&forks[rightFork]);
-        heldBy[rightFork] = thisID;
-        sem_wait(&forks[leftFork]);
-        heldBy[leftFork] = thisID;
-
-        status[thisID] = "Eating";
-        usleep(random() % SLEEP_MAX);   // 随机休眠一段时间
-        status[thisID] = "Terminated";
-
-        sem_post(&forks[leftFork]);
-        heldBy[leftFork] = -1;
-        sem_post(&forks[rightFork]);
-        heldBy[rightFork] = -1;
-    }
+    sem_post(&forks[highIndex]);
+    heldBy[highIndex] = -1;
+    sem_post(&forks[lowIndex]);
+    heldBy[lowIndex] = -1;
 
     pthread_exit(NULL);
 }
